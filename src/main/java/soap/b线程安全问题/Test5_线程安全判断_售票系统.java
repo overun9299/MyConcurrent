2d72d6e15c7a20b3e9411f1df8f0b73f6296f598
overun9299 <overun9299@163.com>
2021-01-10 22:02:22 +0800

    java并发、线程、锁等并发编程

---
+++ src/main/java/soap/b线程安全问题/Test5_线程安全判断_售票系统.java
@@ -0,0 +1,77 @@
+package soap.b线程安全问题;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
+
+@Slf4j(topic = "s.ExerciseSell")
+public class Test5_线程安全判断_售票系统 {
+    public static void main(String[] args) throws InterruptedException {
+        // 模拟多人买票
+        TicketWindow window = new TicketWindow(1000);
+
+        // 所有线程的集合
+        List<Thread> threadList = new ArrayList<>();
+        // 卖出的票数统计
+        List<Integer> amountList = new Vector<>();
+        for (int i = 0; i < 2000; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // 买票
+                int amount = window.sell(random(5));
+
+                // 统计买票数
+                amountList.add(amount);
+            });
+            threadList.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threadList) {
+            thread.join();
+        }
+
+        // 统计卖出的票数和剩余票数
+        log.debug("余票：{}",window.getCount());
+        log.debug("卖出的票数：{}", amountList.stream().mapToInt(i-> i).sum());
+    }
+
+    // Random 为线程安全
+    static Random random = new Random();
+
+    // 随机 1~5
+    public static int random(int amount) {
+        return random.nextInt(amount) + 1;
+    }
+}
+
+// 售票窗口
+class TicketWindow {
+    private int count;
+
+    public TicketWindow(int count) {
+        this.count = count;
+    }
+
+    // 获取余票数量
+    public int getCount() {
+        return count;
+    }
+
+    // 售票
+    public int sell(int amount) {
+        if (this.count >= amount) {
+            this.count -= amount;
+            return amount;
+        } else {
+            return 0;
+        }
+    }
+}

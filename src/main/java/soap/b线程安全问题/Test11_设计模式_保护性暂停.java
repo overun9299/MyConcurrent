d78c23cc654f56abecd3212eca7c335438220190
ZhangPY <18502973371@163.com>
2021-01-14 11:03:57 +0800

    添加超时时间

--- src/main/java/soap/b线程安全问题/Test11_设计模式_保护性暂停.java
+++ src/main/java/soap/b线程安全问题/Test11_设计模式_保护性暂停.java
@@ -14,20 +14,35 @@ import java.util.List;
 @Slf4j(topic = "s.Test11_设计模式_保护性暂停")
 public class Test11_设计模式_保护性暂停 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         GuardedObject guardedObject = new GuardedObject();
         Thread t1 = new Thread(() -> {
             List<String> download = Downloader.download();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             guardedObject.setResponse(download);
         },"t1");
 
         Thread t2 = new Thread(() -> {
-            List<String> response = (List<String>)guardedObject.getResponse();
-            log.debug("获取到的数据为 {}" , response.size());
+            List<String> response = (List<String>)guardedObject.getResponse(10000);
+            if (response != null) {
+                log.debug("获取到的数据为 {}" , response.size());
+            } else {
+                log.debug("没有获取到");
+            }
         },"t2");
+
+        Thread t3 = new Thread(() -> {
+            guardedObject.setResponse(null);
+        },"t3");
 
         t1.start();
         t2.start();
+        Thread.sleep(1000);
+        t3.start();
     }
 
 
@@ -38,6 +53,10 @@ class GuardedObject {
     /** 返回结果 **/
     private Object response;
 
+    /**
+     * 获取返回结果
+     * @return
+     */
     public Object getResponse (){
         synchronized (this) {
             while (response == null) {
@@ -52,8 +71,39 @@ class GuardedObject {
         }
     }
 
+    /**
+     * 获取返回结果,带超时时间
+     * @param timeout 超时时间
+     * @return
+     */
+    public Object getResponse (long timeout){
+        synchronized (this) {
+            /** 经历时间 **/
+            long undergoTime = 0;
+            long inTime = System.currentTimeMillis();
+            while (response == null) {
+                log.debug("获取方法已经历:{}" , undergoTime);
+                if (undergoTime >= timeout) {
+                    /** 等待超时,退出等待 **/
+                    log.debug("等待超时,退出等待");
+                    break;
+                }
+                try {
+                    log.debug("没有返回参数,开始wait");
+                    this.wait(timeout - undergoTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                long outTime = System.currentTimeMillis();
+                undergoTime = outTime-inTime;
+            }
+            return response;
+        }
+    }
+
     public void setResponse(Object response) {
         synchronized (this) {
+            log.debug("放入结果,唤醒所有线程");
             this.response = response;
             this.notifyAll();
         }

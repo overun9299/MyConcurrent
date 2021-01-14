7a17b781d4a7f4c00fe886e1a23fb9c2dd786890
ZhangPY <18502973371@163.com>
2021-01-14 10:16:29 +0800

    设计模式-保护性暂停

--- src/main/java/soap/b线程安全问题/Test11_设计模式_保护性暂停.java
+++ src/main/java/soap/b线程安全问题/Test11_设计模式_保护性暂停.java
@@ -1,6 +1,9 @@
 package soap.b线程安全问题;
 
 import lombok.extern.slf4j.Slf4j;
+import soap.utils.Downloader;
+
+import java.util.List;
 
 /**
  * Created by ZhangPY on 2021/1/13
@@ -11,6 +14,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "s.Test11_设计模式_保护性暂停")
 public class Test11_设计模式_保护性暂停 {
 
+    public static void main(String[] args) {
+        GuardedObject guardedObject = new GuardedObject();
+        Thread t1 = new Thread(() -> {
+            List<String> download = Downloader.download();
+            guardedObject.setResponse(download);
+        },"t1");
+
+        Thread t2 = new Thread(() -> {
+            List<String> response = (List<String>)guardedObject.getResponse();
+            log.debug("获取到的数据为 {}" , response.size());
+        },"t2");
+
+        t1.start();
+        t2.start();
+    }
 
 
 }
--- src/main/java/soap/utils/Downloader.java
+++ src/main/java/soap/utils/Downloader.java
@@ -10,15 +10,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Downloader {
-    public static List<String> download() throws IOException {
-        HttpURLConnection conn = (HttpURLConnection) new URL("https://www.baidu.com/").openConnection();
+    public static List<String> download()  {
         List<String> lines = new ArrayList<>();
-        try (BufferedReader reader =
-                     new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                lines.add(line);
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL("https://www.baidu.com/").openConnection();
+
+            try (BufferedReader reader =
+                         new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    lines.add(line);
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return lines;
     }
